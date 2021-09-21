1da07d6d80a60763c814cc1858f782b0ed8b5c86
Irvin Cortez <irvincotez67@gmail.com>
2021-09-21 16:11:53 -0600

    base workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
 
 function compileSass(){
     return gulp.src('scss/app.scss')
-        .pipe(sass({
-            includedPaths: ['scss']
-        }))
+        .pipe(autoprefixer())
+        .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream())
 }
 
-exports.default = compileSass
+function server() {
+    // Archivos que se incluiran en el servidor
+    browserSync.init({
+        files: ["app/css/*.css", "app/js/*.js", "app/*.html"],
+        server: {
+            baseDir: "app"
+        }
+    })
+
+    gulp.watch(['scss/*.scss'], compileSass)
+    gulp.watch('app/*.html').on('change', browserSync.reload)
+    gulp.watch('app/views/*.html').on('change', browserSync.reload)
+}
+
+exports.default = gulp.series(compileSass, server)
